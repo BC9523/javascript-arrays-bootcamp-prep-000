d7374425e4d6fe02e215228bce3abd845dfb2416
Calypso Day <calypso.day@yahoo.com>
2019-04-08 15:28:43 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
+
+function addElementToBeginningOfArray(arrayName,element){
+ return [element,...arrayName];
+}
+
+function destructivelyAddElementToBeginningOfArray(arrayName,element){
+  arrayName.unshift(element);
+}
